05f146a310b88a5fd241c68f3d7d7ff9c00c4c4c
JMaio <j.f.maio98@gmail.com>
2018-09-26 13:47:00 +0100

    implement mapbox behaviour

--- app/src/main/java/io/github/jmaio/coinz/MainActivity.kt
+++ app/src/main/java/io/github/jmaio/coinz/MainActivity.kt
@@ -12,11 +12,55 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mapView : MapView
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(applicationContext, getString(R.string.app_access_token))
         setContentView(R.layout.activity_main)
 
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync {
+        it.setStyle(Style.MAPBOX_STREETS)
+        }
     }
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
 }

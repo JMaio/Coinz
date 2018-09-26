6987ee951bbba8e9ac434a776f7eed6d5a7e059a
JMaio <j.f.maio98@gmail.com>
2018-09-26 13:46:18 +0100

    optimise imports

--- app/src/main/java/io/github/jmaio/coinz/MainActivity.kt
+++ app/src/main/java/io/github/jmaio/coinz/MainActivity.kt
@@ -1,8 +1,13 @@
 package io.github.jmaio.coinz
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.constants.Style
+import com.mapbox.mapboxsdk.maps.MapView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {

bc6b6518e85b631eab831f2c25dd1fc01bbb6617
JMaio <j.f.maio98@gmail.com>
2018-09-29 16:51:27 +0100

    create coin class

---
+++ app/src/main/java/io/github/jmaio/coinz/Coin.kt
@@ -0,0 +1,29 @@
+package io.github.jmaio.coinz
+
+import android.content.Context
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import io.github.jmaio.coinz.R.color as CoinzColor
+
+class Coin(context : Context, name : String, val chip: Chip) {
+    var visible = true
+
+    val color = when(name) {
+        "SHIL" -> CoinzColor.colorShil
+        "DOLR" -> CoinzColor.colorDolr
+        "QUID" -> CoinzColor.colorQuid
+        "PENY" -> CoinzColor.colorPeny
+        else -> CoinzColor.colorDisabled
+    }
+
+    fun setDayMarketValue() {
+        // TODO: set this to be dynamically allocated
+        var value = ""
+    }
+
+    fun toggleVisibility() {
+        chip.setOnCheckedChangeListener { _, isChecked ->
+            MainActivity().toast("Shil was pressed, it's now $isChecked! ")
+        }
+    }
+}

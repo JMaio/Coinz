532813cf7b1fd9ee8600c2749f0d1c3411ad28ea
JMaio <j.f.maio98@gmail.com>
2018-10-06 22:24:07 +0100

    skeleton definition of wallet object

---
+++ app/src/main/java/io/github/jmaio/coinz/Wallet.kt
@@ -0,0 +1,22 @@
+package io.github.jmaio.coinz
+
+class Wallet {
+    // store a user's gold as Double
+    var gold: Double = 0.0
+
+    // collect or earn coin from another player
+    fun getCoin(coin: Coin) {
+
+    }
+
+    // exchange a coin for its gold value
+    fun bankCoin(coin: Coin) {
+
+    }
+
+    // transfer a coin to another player
+    fun donateCoin(coin: Coin, player: Player) {
+
+    }
+
+}

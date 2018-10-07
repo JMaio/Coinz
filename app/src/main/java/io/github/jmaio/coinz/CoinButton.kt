9299ac99e361c69e5ff5a02ef0bd29f2f8008ad4
JMaio <j.f.maio98@gmail.com>
2018-10-08 00:22:07 +0100

    add logging to button presses

--- app/src/main/java/io/github/jmaio/coinz/CoinButton.kt
+++ app/src/main/java/io/github/jmaio/coinz/CoinButton.kt
@@ -1,12 +1,13 @@
 package io.github.jmaio.coinz
 
 import android.content.Context
-import android.content.res.Resources
 import com.google.android.material.chip.Chip
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
 import org.jetbrains.anko.toast
 import io.github.jmaio.coinz.R.color as CoinzColor
 
-class CoinButton(context: Context, val name: String, val chip: Chip) {
+class CoinButton(context: Context, val name: String, val chip: Chip) : AnkoLogger {
     var visible = true
 
     val color = when(name) {
@@ -18,7 +19,8 @@ class CoinButton(context: Context, val name: String, val chip: Chip) {
     }
     
     val toggle = chip.setOnCheckedChangeListener { buttonView, isChecked ->
-        context.toast("${name} was pressed, now $isChecked")
+        context.toast("$name was pressed, now $isChecked")
+        debug("[coinButton] button '$name' pressed --> $isChecked")
         toggleVisibility()
     }
     

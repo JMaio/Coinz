fa2e4c477f955c9f8d62c4d4413b6ca06f5160c7
JMaio <j.f.maio98@gmail.com>
2018-12-08 18:44:23 +0000

    update coin class

--- app/src/main/java/io/github/jmaio/coinz/Coin.kt
+++ app/src/main/java/io/github/jmaio/coinz/Coin.kt
@@ -1,11 +1,13 @@
 package io.github.jmaio.coinz
 
 data class Coin(
-        val id: String,
-        val currency: String,
-        val value: Double
+        val id: String?,
+        val currency: String?,
+        val value: Double?
 ) {
-    fun toMap(): HashMap<String, Any> {
+    constructor(): this("", "", .0)
+
+    fun toMap(): HashMap<String?, Any?> {
         return hashMapOf(
                 Pair("id", id),
                 Pair("currency", currency),
